6eb5e012129ff542a09ca42949bfbd7e53fadc8c
Johnny <jonysebastin@gmail.com>
2023-12-21 19:35:16 -0800

    Aligning center

--- app/components/ChatContainer.tsx
+++ app/components/ChatContainer.tsx
@@ -1,50 +1,53 @@
 'use client'
-import { Button } from '@chakra-ui/react'
+import { Button, Center, Stack } from '@chakra-ui/react'
 import { SyntheticEvent, useState } from 'react'
 
 const ChatContainer = () => {
   const [inputText, setInputText] = useState('')
   const [chatHistory, setChatHistory] = useState<string[]>([])
 
-  const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault()
-    setChatHistory((prevChatHistory) => [...prevChatHistory, inputText])
+  const handleSubmit = () => {
+    if (inputText.length)
+      setChatHistory((prevChatHistory) => [...prevChatHistory, inputText])
     setInputText('')
   }
 
   return (
     <div className="p-4 bg-gray-200 rounded-md h-screen w-screen">
       <CityPills />
-      <div className="flex flex-col items-end">
+
+      <Stack align="center">
         {chatHistory.map((message, index) => (
           <div key={index} className="bg-white p-4 rounded-md shadow-md">
             <p className="text-lg">{message}</p>
           </div>
         ))}
-
-        <div className="mt-4 absolute bottom-0 ">
-          <input
-            className="px-4 py-2 border border-gray-300 rounded-md"
-            type="text"
-            placeholder="Type your message..."
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-          />
-          <button
-            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
-            onClick={(e) => handleSubmit(e)}
-          >
-            Submit
-          </button>
-        </div>
-      </div>
+        <input
+          className="px-4 py-2 border border-gray-300 rounded-md"
+          type="text"
+          placeholder="Type your message..."
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSubmit()
+            }
+          }}
+        />
+        <button
+          className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
+          onClick={() => handleSubmit()}
+        >
+          Submit
+        </button>
+      </Stack>
     </div>
   )
 }
 
 const CityPills = () => {
   return (
-    <div className="flex flex-row gap-6 justify-center">
+    <Stack direction="row" spacing={4} align="center" justify="center">
       <Button colorScheme="teal" variant="outline">
         San Francisco
       </Button>
@@ -60,7 +63,7 @@ const CityPills = () => {
       <Button colorScheme="teal" variant="outline">
         Pune, India
       </Button>
-    </div>
+    </Stack>
   )
 }
 
