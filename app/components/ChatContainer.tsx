c7a6ee648b22851240e13152bb1f23b3a2537238
Johnny <jonysebastin@gmail.com>
2023-12-27 16:52:28 -0800

    Adding recsAllowed text

--- app/components/ChatContainer.tsx
+++ app/components/ChatContainer.tsx
@@ -1,38 +1,52 @@
 'use client'
 import {
-  Button,
-  Input,
-  Show,
-  Skeleton,
-  Stack,
-  Text,
-  Textarea,
+    Button,
+    Input,
+    Show,
+    Skeleton,
+    Stack,
+    Text,
+    Textarea,
 } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CityPillMainButton } from './CityPillMainButton'
 import { DESIGN_COLORS } from '../constants'
 
 const ChatContainer = () => {
-  const [inputText, setInputText] = useState('')
-  const [isFetching, setIsFetching] = useState(false)
-  const [chatHistory, setChatHistory] = useState<string[]>([])
+    const [inputText, setInputText] = useState('')
+    const [isFetching, setIsFetching] = useState(false)
+    const [chatHistory, setChatHistory] = useState<string[]>([])
+    const [recsAllowed, setRecsAllowed] = useState(true)
 
-  const handleAskMessage = async () => {
-    if (inputText.length) {
-      setChatHistory((prevChatHistory) => [...prevChatHistory, inputText])
-      setInputText('')
-      try {
-        setIsFetching(true)
-        const response = await fetch('/api/ask', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ message: inputText }),
-        })
-        const data = await response.json()
-        setChatHistory((prevChatHistory) => [...prevChatHistory, data.message])
-        setIsFetching(false)
+    useEffect(() => {
+        if(recsAllowed === false) {
+            const timer = setTimeout(() => {
+                setRecsAllowed(true)
+            }, 30000)
+            return () => {
+    
+                if(timer) clearTimeout(timer)
+            }
+        }
+    }, [recsAllowed])
+
+    const handleAskMessage = async () => {
+        if (inputText.length) {
+            setChatHistory((prevChatHistory) => [...prevChatHistory, inputText])
+            setInputText('')
+            try {
+                setIsFetching(true)
+                const response = await fetch('/api/ask', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ message: inputText }),
+                })
+                const data = await response.json()
+                setChatHistory((prevChatHistory) => [...prevChatHistory, data.message])
+                setIsFetching(false)
+                setRecsAllowed(false)
       } catch (e) {
         console.log(e)
       }
@@ -49,7 +63,8 @@ const ChatContainer = () => {
 
   return (
     <Stack align="center" mt={32} marginX={16}>
-      <CityPillMainButton setInputMessage={setInputMessage} />
+
+      {Boolean(recsAllowed) && <CityPillMainButton setInputMessage={setInputMessage} />}
       <Stack className="lg:w-3/4">
         {chatHistory.map((message, index) => (
           <div key={index} className="bg-white rounded-md shadow-md p-5">
@@ -64,6 +79,7 @@ const ChatContainer = () => {
             className="mb-4 p4"
           />
         )}
+        {Boolean(recsAllowed) && (
         <Stack
           spacing={4}
           className="mt-24"
@@ -105,6 +121,14 @@ const ChatContainer = () => {
             Ask Johnny
           </Button>
         </Stack>
+        )}
+
+        {recsAllowed === false && (
+            <Text className="mt-24" fontSize="2xl" color={DESIGN_COLORS.SECONDARY} align='center'>
+                I hope you found Johnnys Recommendation useful! <br /> You can ask Johnny for more recommendations in 1 minute. <br/>
+                Meanwhile checkout <a target='_blank' href="https://foodieyouall.substack.com" className="underline">Johnny`s Substack</a>!
+            </Text>
+            )}
       </Stack>
     </Stack>
   )
