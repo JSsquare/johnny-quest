0466d5683441c8b16105c9acc0c3279dc8bb2567
Johnny Sebastian <jonysebastin@gmail.com>
2025-02-14 07:47:33 -0800

    adding Skipping insert for dev environment

--- pages/api/accessed-user.ts
+++ pages/api/accessed-user.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import supabase from "../../utils/supabaseClient"
 import { handleError, validateMethod } from "../../utils/apiHelpers"
 import { logError, logInfo } from "../../utils/logger"
+import { AccessedUserTypes } from "@/app/types/servicesTypes"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!validateMethod(req, res, "POST")) return
@@ -13,6 +14,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
+    if (process.env.NODE_ENV === "development" && user_type === AccessedUserTypes.PASSKEY_CODE) {
+      logInfo("Skipping insert accessed user query in development environment for user_type passkey_code")
+      return res.status(200).json({ message: "Skipped insert accessed user  in development environment", data: { success: 'OK' } })
+    }
+
     const { error } = await supabase.from("accessed_users").insert([{ user_type, value }])
 
     if (error) {
