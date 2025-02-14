7bda799d0bdc74bc1ea1275e7f2b05d36a612ab3
Johnny Sebastian <jonysebastin@gmail.com>
2025-02-13 22:00:29 -0800

    Not allowing users to by pass by Modal DOM Deletion

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,30 @@ import PassKeyModal from './components/PassKeyModal';
 import { SHOW_PASSKEY } from './constants/configConstants';
 import Cookies from "js-cookie";
 
-
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
+  const [isModalClosedSuccessfully, setIsModalClosedSuccessfully] = useState(false);
 
   useEffect(() => {
     // Ensure cookies are read only on the client side
     const shouldOpenModal = Cookies.get("passkeyModalClosed") !== "true";
     if (SHOW_PASSKEY && shouldOpenModal) {
       setShowModal(true);
+    } else {
+      setIsModalClosedSuccessfully(true);
     }
   }, []);
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setIsModalClosedSuccessfully(true);
+  };
 
   return (
     <ChakraProvider>
       <main className="flex flex-col font-sans content-center">
-        {showModal && <PassKeyModal onClose={() => setShowModal(false)} />}
-        <MainChatContainer />
+        {showModal && <PassKeyModal onClose={handleModalClose} />}
+        {isModalClosedSuccessfully && <MainChatContainer />}
       </main>
     </ChakraProvider>
   );
