eda4bc9d688aa011a5e218f9a410ebfe287d1a98
Johnny Sebastian <jonysebastin@gmail.com>
2025-05-10 17:13:46 -0700

    renaming setOpenModal

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,28 @@ import { SHOW_PASSKEY } from './constants/configConstants';
 import Cookies from "js-cookie";
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const [isModalClosedSuccessfully, setIsModalClosedSuccessfully] = useState(false);
 
   useEffect(() => {
     // Ensure cookies are read only on the client side
     const shouldOpenModal = Cookies.get("passkeyModalClosed") !== "true";
     if (SHOW_PASSKEY && shouldOpenModal) {
-      setShowModal(true);
+      setOpenModal(true);
     } else {
       setIsModalClosedSuccessfully(true);
     }
   }, []);
 
   const handleModalClose = () => {
-    setShowModal(false);
+    setOpenModal(false);
     setIsModalClosedSuccessfully(true);
   };
 
   return (
     <ChakraProvider>
       <main className="flex flex-col font-sans content-center">
-        {showModal && <PassKeyModal onClose={handleModalClose} />}
+        {openModal && <PassKeyModal onClose={handleModalClose} />}
         {isModalClosedSuccessfully && <MainChatContainer />}
       </main>
     </ChakraProvider>
