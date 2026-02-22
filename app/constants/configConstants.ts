3ee16d22a686c13eb98e92f3164c696265030b74
Johnny Sebastian <jonysebastin@gmail.com>
2026-02-22 11:13:09 -0800

    Disabling show passkey

--- app/constants/configConstants.ts
+++ app/constants/configConstants.ts
@@ -1,5 +1,5 @@
 export const BLOCK_REQUEST: boolean = false
 export const TEST_MODE: boolean = false
-export const SHOW_PASSKEY: boolean = true
+export const SHOW_PASSKEY: boolean = false
 export const ENABLE_SPECIFIC_QUESTION: boolean = false
 export const TURN_ON_DARK_MODE: boolean = false
